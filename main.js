734135b07b4d3fb12d43cab5e7e1f59225806c6e
qkkehugx <renaldasjakas@gmail.com>
2020-04-05 00:23:51 +0100

    minor changes to main

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const dir = __dirname;
+
+app.set('view engine', 'pug');
+app.set('views', `${dir}/views`);
+
+
 module.exports = app;
